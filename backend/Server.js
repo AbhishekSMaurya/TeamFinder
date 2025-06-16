f92c76085959b78d7e59a488c500a2991dc77d93
Abhishek Maurya <ab2005maurya@gmail.com>
2025-06-16 16:54:09 +0530

    fixed server

--- backend/Server.js
+++ backend/Server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
-const path = require("path");
 const axios = require('axios');
 const app = express();
 const PORT =  process.env.PORT || 5000;
